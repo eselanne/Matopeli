ba2b0d57be79901234c292bcf18dd8820e771564
Elmeri Selänne <eljuse@utu.fi>
2019-04-20 00:15:44 +0300

    Ohjelman ikkuna lisätty

--- src/MainMenu.java
+++ src/MainMenu.java
@@ -1,4 +1,18 @@
+import javax.swing.JFrame;
 
 public class MainMenu {
-
+	
+	public MainMenu() {
+		
+	
+	
+	JFrame frame = new JFrame();
+	
+	frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	frame.setTitle("Matopeli");
+	frame.setSize(700, 700);
+	frame.setVisible(true);
+	
+	
+	}
 }
