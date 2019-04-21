6e4de3fd53605a0abb5f6fe96ad5e10718401bc3
Marko Järvinen <mabeja@utu.fi>
2019-04-21 19:00:55 +0300

    Lisätty päävalikko

--- src/Game.java
+++ src/Game.java
@@ -1,13 +1,16 @@
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
+import java.awt.Rectangle;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 import java.util.ArrayList;
 
 import javax.swing.JPanel;
 
-public class Game extends JPanel implements Runnable, KeyListener {
+public class Game extends JPanel implements Runnable, KeyListener, MouseListener {
 	
 	private static final long serialVersionUID = 1L;
 	public static final int WIDTH = 500, HEIGHT = 500;
@@ -19,19 +22,21 @@ public class Game extends JPanel implements Runnable, KeyListener {
 	private int xCoord = 10, yCoord = 10, size = 5;
 	private int ticks = 0;
 	
+	private MainMenu menu = new MainMenu();
+	
 	// kuvaa ohjelman tilaa
 	private enum STATE{
-		GAME,
-		MENU
+		GAME, MENU
 	};
 	
 	// ohjelman tila
-	// private STATE State = STATE.MENU;
+	private STATE State = STATE.MENU;
 	
 	public Game() {
 		setFocusable(true);
 		setPreferredSize(new Dimension(WIDTH, HEIGHT));
 		addKeyListener(this);
+		addMouseListener(this);
 		snake = new ArrayList<Snakeblock> ();
 		start();
 	}
@@ -54,36 +59,36 @@ public class Game extends JPanel implements Runnable, KeyListener {
 	}
 	
 	public void tick () {
-		if(snake.size() ==0 ) {
-			s = new Snakeblock(xCoord,yCoord,10);
-			snake.add(s);
-		}
-		ticks++;
-		if(ticks > 250000) {
-			if (right) {
-				xCoord++;
+		if(State == STATE.GAME) {
+			if(snake.size() ==0 ) {
+				s = new Snakeblock(xCoord,yCoord,10);
+				snake.add(s);
 			}
-			if(left) {
-				xCoord--;
+			ticks++;
+			if(ticks > 250000) {
+				if (right) {
+					xCoord++;
+				}
+				if(left) {
+					xCoord--;
+				}
+				if (up) {
+					yCoord--;
+				}
+				if (down) {
+					yCoord++;
+				}
+				
+				ticks = 0;
+				
+				s = new Snakeblock(xCoord, yCoord, 10);
+				snake.add(s);
+				
+				if (snake.size() > size) {
+					snake.remove(0);
+				}
 			}
-			if (up) {
-				yCoord--;
-			}
-			if (down) {
-				yCoord++;
-			}
-			
-			ticks = 0;
-			
-			s = new Snakeblock(xCoord, yCoord, 10);
-			snake.add(s);
-			
-			if (snake.size() > size) {
-				snake.remove(0);
-			}
-		}
-		
-		
+		}		
 	}
 	
 	public void paint(Graphics g) {
@@ -91,18 +96,20 @@ public class Game extends JPanel implements Runnable, KeyListener {
 		g.setColor(Color.black);
 		g.fillRect(0, 0, WIDTH, HEIGHT);
 		
-		for(int i = 0; i < WIDTH/10; i++) {
-			g.drawLine(i*10, 0, i*10, HEIGHT);
-		}
-		for(int i = 0; i < HEIGHT/10; i++) {
-			g.drawLine(0, i*10,HEIGHT , i*10);
-		}
-		for (int i = 0; i < snake.size(); i++) {
-			snake.get(i).draw(g);
-		}
+		if(State == STATE.GAME) {
 		
-		
-		
+			for(int i = 0; i < WIDTH/10; i++) {
+				g.drawLine(i*10, 0, i*10, HEIGHT);
+			}
+			for(int i = 0; i < HEIGHT/10; i++) {
+				g.drawLine(0, i*10,HEIGHT , i*10);
+			}
+			for (int i = 0; i < snake.size(); i++) {
+				snake.get(i).draw(g);
+			}
+		}else if(State == STATE.MENU) {
+			menu.render(g);
+		}
 	}
 	
 	@Override 
@@ -116,29 +123,32 @@ public class Game extends JPanel implements Runnable, KeyListener {
 	@Override
 	public void keyPressed(KeyEvent e) {
 		int key = e.getKeyCode();
-		if(key == KeyEvent.VK_RIGHT && !left) {
-			right = true;
-			up = false;
-			down = false;
-		}
-		if (key == KeyEvent.VK_LEFT && !right) {
-			left = true;
-			up = false;
-			down = false;
-			
-		}
-		if (key == KeyEvent.VK_UP && !down) {
-			up = true;
-			left = false;
-			right = false;
-			
-		}
-		if (key == KeyEvent.VK_DOWN && !up) {
-			down = true;
-			left = false;
-			right = false;
 		
+		if(State == STATE.GAME) {
+			if(key == KeyEvent.VK_RIGHT && !left) {
+				right = true;
+				up = false;
+				down = false;
+			}
+			if (key == KeyEvent.VK_LEFT && !right) {
+				left = true;
+				up = false;
+				down = false;
+				
+			}
+			if (key == KeyEvent.VK_UP && !down) {
+				up = true;
+				left = false;
+				right = false;
+				
+			}
+			if (key == KeyEvent.VK_DOWN && !up) {
+				down = true;
+				left = false;
+				right = false;
 			
+				
+			}
 		}
 		
 	}
@@ -151,6 +161,61 @@ public class Game extends JPanel implements Runnable, KeyListener {
 
 	@Override
 	public void keyTyped(KeyEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseClicked(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseExited(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mousePressed(MouseEvent e) {
+		int mx = e.getX();
+		int my = e.getY();
+		
+		// pelaa-nappi
+		if(mx >= Game.WIDTH/4 + 70 && mx <= Game.WIDTH/4 + 70 + 100) {
+			if(my >= 150 && my <= 200) {
+				// painettu play-nappi
+				State = STATE.GAME;
+			}
+		}
+		
+		// highscore-nappi
+		if(mx >= Game.WIDTH/4 + 15 && mx <= Game.WIDTH/4 + 15 + 210) {
+			if(my >= 250 && my <= 300) {
+				// painettu highscore-nappi
+				//State = STATE.HIGHSCORE;
+			}
+		}
+		
+		// poistu-nappi
+		if(mx >= Game.WIDTH/4 + 70 && mx <= Game.WIDTH/4 + 70 + 100) {
+			if(my >= 350 && my <= 400) {
+				// painettu poistu-nappi
+				System.exit(1);
+			}
+		}
+		
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent arg0) {
 		// TODO Auto-generated method stub
 		
 	}
--- src/MainMenu.java
+++ src/MainMenu.java
@@ -1,11 +1,34 @@
-import javax.swing.JFrame;
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Rectangle;
 
 public class MainMenu {
 	
-	public MainMenu() {
+	public Rectangle playNappi = new Rectangle(Game.WIDTH/4 + 70, 150, 100, 50);
+	public Rectangle highScoreNappi = new Rectangle(Game.WIDTH/4 + 15, 250, 210, 50);
+	public Rectangle exitNappi = new Rectangle(Game.WIDTH/4 + 70, 350, 100, 50);
+	
+	public void render(Graphics g) {
 		
-	
-	
-	
+		// otsikko
+		Font font0 = new Font("century gothic", Font.BOLD, 50);
+		g.setFont(font0);
+		g.setColor(Color.white);
+		g.drawString("MATOPELI", Game.WIDTH/4, 100);
+		
+		// nappien tekstit
+		Font font1 = new Font("arial", Font.BOLD, 25);
+		g.setFont(font1);
+		g.drawString("Pelaa", playNappi.x + 18, playNappi.y + 35);
+		g.drawString("Parhaat tulokset", highScoreNappi.x + 10, highScoreNappi.y + 35);
+		g.drawString("Poistu", exitNappi.x + 14, exitNappi.y + 35);
+		
+		// nappien laatikot
+		Graphics2D g2d = (Graphics2D) g;
+		g2d.draw(playNappi);
+		g2d.draw(highScoreNappi);
+		g2d.draw(exitNappi);
 	}
 }
