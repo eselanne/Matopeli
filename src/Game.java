62674505682a01b6ea0bba6ed49a23a1d49cb3f4
Marko Järvinen <mabeja@utu.fi>
2019-04-21 17:05:34 +0300

    Lisätty enum STATE ohjelman tilaa kuvaamaan

--- src/Game.java
+++ src/Game.java
@@ -15,6 +15,15 @@ public class Game extends JPanel implements Runnable {
 	private ArrayList<Snakeblock> snake;
 	private int xCoord = 10, yCoord = 10, size = 5;
 	
+	// kuvaa ohjelman tilaa
+	private enum STATE{
+		GAME,
+		MENU
+	};
+	
+	// ohjelman tila
+	// private STATE State = STATE.MENU;
+	
 	public Game() {
 		setPreferredSize(new Dimension(WIDTH, HEIGHT));
 	}
